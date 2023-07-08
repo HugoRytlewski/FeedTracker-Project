e3161e98b4f1068958022c2c16a54fd3a45e617e
Hugo <rytlewski.hugo@etu.educ-valadon-limoges.fr>
2023-07-08 23:25:37 +0200

    dd

--- app.vue
+++ app.vue
@@ -87,7 +87,7 @@ async function setRssFeed(dataFeed:any, limit:boolean) {
   const items = xml.getElementsByTagName('item');
   let limitLength;
   if (limit) {
-    limitLength = limiteArticles;
+    limitLength = limiteArticles ;
   } else {
     limitLength = items.length;
   }
