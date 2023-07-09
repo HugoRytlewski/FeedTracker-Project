aafe2cf15315aebd1215ae33d1e29a4a5c44bff2
Hugo <rytlewski.hugo@etu.educ-valadon-limoges.fr>
2023-07-09 14:12:57 +0200

    dddd

--- app.vue
+++ app.vue
@@ -10,6 +10,7 @@ import { useWindowScroll } from "@vueuse/core";
 
 
 const articles = ref([]);
+const limitArticles = ref(10);
 const limiteArticles = ref(10);
 const { y } = useWindowScroll();
 const waitLoadRss = ref(true)
@@ -48,8 +49,6 @@ let tableau = [
 
 onMounted(async () => {
   try {
-
-
     await FeedArticles(tableau);
     
   } catch (erreur) {
@@ -92,6 +91,8 @@ function extractImageSource(contentEncoded) {
   }
 }
 async function setRssFeed(dataFeed:any, limit:boolean) {
+  limitArticles.value = 10 ;
+
   const parser = new DOMParser();
   const xml = parser.parseFromString(dataFeed, 'text/xml');
   const items = xml.getElementsByTagName('item');
@@ -185,11 +186,14 @@ window.scrollTo({
 <template>
     <Wait class="hidden md:grid		"  v-if="waitLoadRss"/>
     <Navbar/>
+    <div :class="[limitArticles < articles.length ? 'pb-px' : '','p-6']">
+
+
   <div class="p-10 mt-24 lg:p-28 lg:mt-16">
     <div class="grid lg:grid-cols-2 gap-16 ">
       <div v-for="(article , index) in articles.slice(0, limiteArticles)" :key="index" class="flex h-full items-center justify-center flex-col gap-6 rounded-xl bg-neutral-800 hover:-translate-y-1 hover:scale-105  duration-200 p-6  border-neutral-800 shadow-md ">
-        <img  v-if="article.img" :src="article.img" class="hidden md:block pixelated-image  w-11/12 max-h-72 rounded-lg object-cover  lazyload">
-        <img v-else src="~/assets/img/ee.png" class="hidden md:block  w-11/12 max-h-72 rounded-lg object-cover ">
+        <img  v-if="article.img" :src="article.img" class="pixelated-image  w-11/12 max-h-72 rounded-lg object-cover  lazyload">
+        <img v-else src="~/assets/img/ee.png" class=" w-11/12 max-h-72 rounded-lg object-cover ">
         <div class="flex flex-col justify-between w-full	 gap-y-4">
           <div class="space-y-4">
             <div class="flex justify-center">
@@ -224,7 +228,8 @@ window.scrollTo({
     </button>
   </Transition>
   <Footer/>
-  
+</div>
+
 </template>
 
 <style>
