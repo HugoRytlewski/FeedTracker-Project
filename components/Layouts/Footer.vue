998d521955234031061c9e7ef52d6e3c14ee8e9a
Hugo <rytlewski.hugo@etu.educ-valadon-limoges.fr>
2023-10-07 17:44:18 +0200

    dzdz

--- components/Layouts/Footer.vue
+++ components/Layouts/Footer.vue
@@ -1,13 +1,18 @@
-<template> 
-    <footer class="bg-neutral-900 rounded-lg m-4 ">
-        <div class="w-screen-xl items-center p-4 flex flex-col md:flex-row md:justify-center">
-            <span class="text-md text-white sm:text-center ">© 2023 
-                <a class="">FeedTracker, Tous droits réservés</a>
-        
-            </span>
-           
-        </div>
-    </footer>
-
+<template>
+  <footer class="bg-neutral-900 rounded-lg m-4">
+    <div
+      class="w-screen-xl items-center p-4 flex flex-col md:flex-row md:justify-center"
+    >
+      <span class="text-md text-white sm:text-center"
+        >© 2023
+        <a class=""
+          >FeedTracker,
+          <a href="hugorytlewski.com" target="_blank" class="hover:underline"
+            >Hugo Rytlewski</a
+          >
+          Tous droits réservés</a
+        >
+      </span>
+    </div>
+  </footer>
 </template>
-    
